94267ff44f8ac765415d2997ac19ea161463ad99
Andrey Yudin <dj402and@gmail.com>
2018-07-05 09:41:49 +0300

    lil.js fix

--- cmd/lol.js
+++ cmd/lol.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 exports.run = (msg, args) => {
     fs.readFile('./img/' + args[1] + '.png', (err, data) => {
-        const text = '';
+        let text = '';
         const list = ['cry', 'dog', 'kojima', 'pain'];
 
-        for (let i = 0; i < list.length; i++) {
-            text += list[i] + '\n';
-        }
+        list.forEach((i) => {
+            text += i + '\n';
+        });
 
         if (err) msg.channel.send(text);
         msg.channel.send('', {file: data});
